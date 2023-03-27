40c12ebf7cce259d55ddc53cc1e7d68fc0b307d9
Oh Daehyeon <xzdheogus1@gmail.com>
2023-03-27 23:19:03 +0900

    v1.0.3

--- index2.js
+++ index2.js
@@ -13,10 +13,34 @@ function simpleGame(x) {
 }
 simpleGame("가위");
 // as const 문법
-//
+// object value 값을 그대로 타입으로 지정해준다.
+// object 속성들에 모두 readonly를 붙여준다.
 let testData = {
     name: 'kim'
 };
 function myFn(a) {
 }
 myFn(testData.name);
+let myFn2 = function (a) {
+    return 10;
+};
+let 회원정보 = {
+    name: 'kim',
+    age: 30,
+    plusOne(x) {
+        return x + 1;
+    },
+    changeName: () => {
+        console.log('안녕');
+    }
+};
+let cutZero = (x) => {
+    if (x[0] === "0")
+        return x.slice(1);
+    return x;
+};
+console.log(cutZero("0123"));
+let removeDash = (x) => {
+    return parseFloat(x.replace(/-/g, ''));
+};
+console.log(removeDash("010-0000-3333"));
--- index2.ts
+++ index2.ts
@@ -16,7 +16,8 @@ function simpleGame(x :"가위" | "바위" | "보") :("가위"|"바위"|"보")[]
 simpleGame("가위");
 
 // as const 문법
-//
+// object value 값을 그대로 타입으로 지정해준다.
+// object 속성들에 모두 readonly를 붙여준다.
 let testData = {
     name : 'kim'
 } as const
@@ -25,3 +26,45 @@ function myFn(a:'kim'){
 
 }
 myFn(testData.name);
+
+
+type FnType = (a :string) => number;
+
+let myFn2 :FnType = function (a){
+    return 10;
+}
+
+type 회원정보타입 = {
+    name : string,
+    age : number,
+    plusOne : (x :number) => number,
+    changeName : () => void,    
+};
+
+let 회원정보 :회원정보타입 = {
+    name : 'kim',
+    age : 30,
+    plusOne (x){
+      return x + 1
+    },
+    changeName : () => {
+      console.log('안녕')
+    }
+  }
+
+
+
+type CutZeroType = (a :string) => string;
+type RemoveDashType = (a :string) => number;
+
+let cutZero :CutZeroType = (x)=>{
+    if(x[0] === "0") return x.slice(1);
+    return x;
+};
+console.log(cutZero("0123"));
+
+let removeDash :RemoveDashType = (x)=>{ 
+    return parseFloat(x.replace(/-/g, ''));
+};
+console.log(removeDash("010-0000-3333"));
+
