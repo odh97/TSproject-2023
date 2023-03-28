d22cc88c359051459cf0aa9103391140ac6235c0
Oh Daehyeon <xzdheogus1@gmail.com>
2023-03-28 23:23:55 +0900

    v1.0.4

--- index3.ts
+++ index3.ts
@@ -144,13 +144,6 @@ interface Cart extends AddCart1 {
 let 장바구니 :Cart[] = [ { product : '청소기', price : 7000 }, { product : '삼다수', price : 800 }, { product : '청소기', price : 7000, card : false } ];
 
 
-// (숙제4) object 안에 함수를 2개 넣고 싶은데요 
-
-// 1. 이 object 자료는 plus() 함수를 내부에 가지고 있으며 plus 함수는 파라미터 2개를 입력하면 더해서 return 해줍니다. 
-// 2. 이 object 자료는 minus() 함수를 내부에 가지고 있으며 minus 함수는 파라미터 2개를 입력하면 빼서 return 해줍니다. 
-// 이 object 자료를 어떻게 만들면 될까요? 
-// interface를 이용해서 object에 타입지정도 해보십시오. 
-
 interface ObjFn{
   plus : (x:number, y:number) => number,
   minus : (x:number, y:number) => number,
