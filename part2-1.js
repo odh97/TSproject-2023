479d1b89719337176d8fc7b675a25e903f1d8f0d
Oh Daehyeon <xzdheogus1@gmail.com>
2023-03-31 22:04:51 +0900

    v1.0.6

--- part2-1.js
+++ part2-1.js
@@ -29,3 +29,57 @@ function arrFn([a, b, c]) {
     console.log(a, b, c);
 }
 arrFn([40, 'wine', false]);
+// Narrowing 방법들
+function 함수(a) {
+    if (a && typeof a === "string") { } // && 연산자
+}
+function 함수2(animal) {
+    if ('swim' in animal) { //  in 연산자
+        animal.swim;
+    }
+}
+let date = new Date();
+if (date instanceof Date) { }
+; // instanceof 연산자
+function 함수3(x) {
+    if (x.wheel === '2개') {
+        console.log('x는 BikeNarro type이다.');
+    }
+}
+// public private protected static
+// public (기본값으로 설정되어있다.)
+// 수정을 가능하게 해준다.
+class UserPb {
+    name;
+    constructor(a) {
+        this.name = a;
+    }
+}
+let userPb = new User('park');
+userPb.name = 'Hello';
+// 활용1
+class PersonPb {
+    name;
+    constructor(name) {
+        this.name = name;
+    }
+}
+new PersonPb('kim');
+// private
+// 다른곳에서 수정이 불가능하다.
+// ex1) react-redux 같은곳에서 자주 사용한다.
+// ex2) 외부에서 수정 및 변경을 막아준다.
+class UserPriv {
+    name;
+    familyName = "kim";
+    constructor(a) {
+        this.name = a + this.familyName;
+    }
+    nameChange() {
+        this.familyName = 'park';
+    }
+}
+let userPriv = new UserPriv('민수');
+// userPriv.familyName = 'Hello'; (error)
+console.log(userPriv);
+userPriv.nameChange(); // 수정함수실행
--- part2-1.ts
+++ part2-1.ts
@@ -47,3 +47,79 @@ function arrFn([a,b,c] :(string | number | boolean)[]){
 }
 
 arrFn([40, 'wine', false]);
+
+// Narrowing 방법들
+function 함수(a: string | undefined){
+  if( a && typeof a === "string" ){} // && 연산자
+}
+
+type Fish = {swim :string};
+type Bird = {fly :string}
+function 함수2(animal : Fish | Bird){
+  if('swim' in animal){ //  in 연산자
+    animal.swim;
+  }
+}
+
+let date = new Date();
+if(date instanceof Date){}; // instanceof 연산자
+
+type CarNarro = {
+  wheel : '4개',
+  color : string
+}
+type BikeNarro = {
+  wheel : '2개',
+  color : string
+}
+function 함수3(x : CarNarro | BikeNarro){
+  if(x.wheel === '2개'){
+    console.log('x는 BikeNarro type이다.');
+  }
+}
+
+// public private protected static
+// public (기본값으로 설정되어있다.)
+// 수정을 가능하게 해준다.
+
+class UserPb{
+  name : string;
+  constructor(a :string){
+    this.name = a;
+  }
+}
+
+let userPb = new User('park');
+userPb.name = 'Hello'
+
+// 활용1
+class PersonPb {
+  constructor(public name:string){ //파라미터 값을 바로 지정해줄 수 있다.
+
+  }
+}
+new PersonPb('kim');
+
+// private
+// 다른곳에서 수정이 불가능하다.
+// ex1) react-redux 같은곳에서 자주 사용한다.
+// ex2) 외부에서 수정 및 변경을 막아준다.
+
+class UserPriv{
+  name : string;
+  private familyName : string = "kim";
+
+  constructor(a :string){
+    this.name = a + this.familyName;
+  }
+
+  nameChange(){ // 만약 private을 변경하고 싶으면 수정 함수를 만들어서 사용하면 된다.
+    this.familyName = 'park';
+  }
+
+}
+
+let userPriv = new UserPriv('민수');
+// userPriv.familyName = 'Hello'; (error)
+console.log(userPriv);
+userPriv.nameChange(); // 수정함수실행
